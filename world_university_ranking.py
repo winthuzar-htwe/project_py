dedba46bd5c2e4de092a0d773c2f4bc22a6f9112
winthuzar-htwe <69594121+winthuzar-htwe@users.noreply.github.com>
2024-02-15 19:49:52 +0630

    Update world_university_ranking.py

--- world_university_ranking.py
+++ world_university_ranking.py
@@ -6,7 +6,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import numpy as np
 import pycountry
 import altair as alt
